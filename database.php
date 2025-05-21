215054656bd14260519c3aa3c730c0102aec3ad9
Fatom Al-Ghanmi <fatom.alghanmi@triosstudent.com>
2025-05-21 12:08:16 -0400

    added database, and database error files

---
+++ database.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+    $dsn = 'mysql:host=localhost;dbname=book_manager'; // <-- updated database name
+    $username = 'root'; // use your DB username
+    $password = '';     // use your DB password
+
+    try {
+        $db = new PDO($dsn, $username, $password);
+    }
+    catch (PDOException $e)
+    {
+        $_SESSION["database_error"] = $e->getMessage();
+        $url = "database_error.php";
+        header("Location: " . $url);
+        exit();
+    }
+?>
--- index.php
+++ index.php
@@ -1,15 +1,52 @@
+<?php
+    session_start();
+    require("database.php");
+    $queryBooks = 'SELECT * FROM books';
+    $statement1 = $db->prepare($queryBooks);
+    $statement1->execute();
+    $books = $statement1->fetchAll();
+    $statement1->closeCursor();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Assignment 1</title>
+    <link rel="stylesheet" type="txt/css" href="css/main.css" />
+    <title>PHP Assignment 1 - Home</title>
 </head>
 <body>
-    <h1>Book List </h1>
-    <?php
-        echo "Hello, World! This is your first PHP assignment.";
-    ?>
+    <?php include("header.php"); ?>
+?>
+
+    <main>
+        <h1>Book List Manager</h1>
+        <table>
+    <tr>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Genre</th>
+        <th>ISBN</th>
+        <th>Price</th>
+        <th>Published Date</th>
+    </tr>
+
+    <?php foreach ($books as $book): ?>
+        <tr>
+            <td><?php echo $book['title']; ?></td>
+            <td><?php echo $book['author']; ?></td>
+            <td><?php echo $book['genre']; ?></td>
+            <td><?php echo $book['isbn']; ?></td>
+            <td><?php echo $book['price']; ?></td>
+            <td><?php echo $book['published_date']; ?></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
+
+
+    </main>
+
+    <?php include("footer.php"); ?>
 </body>
 
 </html>
