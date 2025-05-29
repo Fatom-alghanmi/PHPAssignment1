0b0905260d4abfee33872cb5b835167b68e6f229
Fatom Al-Ghanmi <fatom.alghanmi@triosstudent.com>
2025-05-29 10:36:47 -0400

    updated css

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <link rel="stylesheet" type="text/css" href="css/main.css"/>
     <title>PHP Assignment 1 - Home</title>
 </head>
 <body>
@@ -28,6 +28,7 @@
         <th>ISBN</th>
         <th>Price</th>
         <th>Published Date</th>
+        <th>Image</th>
         <th>&nbsp;</th> <!-- for edit button -->
         <th>&nbsp;</th> <!-- for delete button -->
 
@@ -45,14 +46,14 @@
             <td><img src="<?php echo htmlspecialchars('./images/' . $book['imageName']); ?>"
                             alt="<?php echo htmlspecialchars('./images/' . $book['imageName']); ?>" style="width:100px; height:auto;" /></td>
             <td>
-                            <form action="update_book_form.php" method="post">
+                            <form action="update_book_form.php" method="post"> 
                                 <input type="hidden" name="book_id"
                                     value="<?php echo $book['bookID']; ?>" />
                                 <input type="submit" value="Update" />
                             </form>
                         </td> <!-- for edit button -->
                         <td>
-                            <form action="delete_book.php" method="post">
+                            <form action="delete_book.php" method="post"> 
                                 <input type="hidden" name="book_id"
                                     value="<?php echo $book['bookID']; ?>" />
                                 <input type="submit" value="Delete" />
@@ -60,11 +61,10 @@
                         </td> <!-- for delete button -->
      </tr>
     <?php endforeach; ?>
-</table>
-
-
+        </table>
     </main>
-    <p><a href="add_book_form.php">Add Book</a></p>
+    <p><a href="add_book_form.php" class="add-book-main">Add Book</a></p>
+
     <?php include("footer.php"); ?>
 </body>
 
